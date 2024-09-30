94c35edb15377b5ebb78a5087065804746d915c3
swordlegend32 <giopro1986@gmail.com>
2024-09-30 22:30:52 +0400

    first commit

---
+++ day2/lesson2.py
@@ -0,0 +1,11 @@
+name = " გიორგი "
+surname = " თედოზაშვილი "
+
+age = " 15 "
+
+st1 = "ჩემი სახელია"
+st2 = "ჩემი გვარია"
+st3 = "მე ვარ"
+st4 = "წლის"
+
+print(st1 + name + st2 + surname + st3 + age + st4)
