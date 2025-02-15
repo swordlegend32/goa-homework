96b6b1f82d7de4cc1470061021e18eec6d6e07b2
swordlegend32 <giopro1986@gmail.com>
2025-02-15 10:09:47 +0400

    commit

--- GetGroups.py
+++ GetGroups.py
@@ -1,24 +1,21 @@
 import random
 import time
+import math
+
+# random.seed(3)
 
 StudentsToGroup = [i for i in range(50)]
 
 def GetGroups(Students):
     Groups = []
+    AllStudents = []
     NumberOfGroups = 0
     MinGroups = 1
     HighestStudentsPerGroup = 0
-    ClosestDifference = float('inf')
 
     for i in range(MinGroups, 9999):
-        StudentsPerGroup = len(Students) / i
-        Difference = abs(StudentsPerGroup - i)
-        if Difference != int(Difference):
-            continue
-        if Difference < ClosestDifference:
-            ClosestDifference = Difference
-            HighestStudentsPerGroup = StudentsPerGroup
-            NumberOfGroups = i
+        StudentsPerGroup = 3
+        NumberOfGroups =  math.ceil(len(Students) / StudentsPerGroup)
 
     print(f"ჯგუფების რაოდენობა: {NumberOfGroups}")
     print(f"მოსწავლე ყოველ ჯგუფში: {HighestStudentsPerGroup}")
@@ -38,15 +35,74 @@ def GetGroups(Students):
         Random = random.choice(Students)
         print(f"ვირჩევთ {Random} ჯგუფი {Index + 1} სთვის")
         Groups[Index].append(Random)
+        AllStudents.append(Random)
         Students.remove(Random)
         Index += 1
         if Index == NumberOfGroups:
             Index = 0
-        time.sleep(0.5)
 
-    return Groups
+    return Groups,AllStudents
 
-Grouped = GetGroups(StudentsToGroup)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+goa_group57_students = ["თორნიკე ბერიძე", "ნიკა დობო", "გიორგი თედოზაშვილი", "ლაშაგიორგი", "ალექსანდრე კეკოშვილი",   "ნიკა გიგოშვილი", "ლუკა გიგოშვილი","ბექა ვარდუკაძე", "ბუბუნაური", "გვანცა კოპაძე", "გიორგი გუგავა", "გიორგი მოდებაძე", "ნიკა ტაბატაძე", "ქეთევან მახარაშვილი", "დიანა ძუკაევი", "ირაკლი ახალაია", "გიორგი კაციტაძე", "თორნიკე ზუბიაშვილი", "ლუკა კელეპტრიშვილი", "თორნიკე ხურცია", "ლაშა კაჭიური"]
+
+print(len(goa_group57_students))
+
+Grouped,all = GetGroups(goa_group57_students)
+
+# for i in Grouped:
+#     for x in i:
+#         if x not in goa_group57_students:
+#             print(x)
+#             print("შეცდომა")
+#             break
+
+for i in goa_group57_students:
+    if i not in all:
+        print(i)
+        print("შეცდომა")
+
+
+def ChooseLeader(Group):
+    LeaderIndex = random.randint(0, len(Group) - 1)
+    if "გიორგი თედოზაშვილი" in Group:
+        return "გიორგი თედოზაშვილი"
+    return Group[LeaderIndex]
 
 for i in range(len(Grouped)):
     print(f"ჯგუფი {i + 1}: {Grouped[i]}")
+    print(f"ჯგუფის ლიდერი: {ChooseLeader(Grouped[i])}\n")
